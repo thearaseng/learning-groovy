3505c727ec3eadf445f0230c26d0d0147f9e0b16
Theara SENGG <sengg.theara@ABAHUBMACDIGTY.local>
2022-09-29 22:22:21 +0700

    MySecondGroovy

---
+++ learning-groovy/src/com/thearaseng/groovy/MySecondGroovy.groovy
@@ -0,0 +1,18 @@
+package com.thearaseng.groovy
+
+def name = 'Theara'
+
+println name
+
+//***************************
+def (a,b,c) = [1,'hello',2.3]
+
+println a
+println b
+println c
+//***************************
+
+double thisIsDouble = 3.4
+String thisIsAString = 'hello'
+
+//***************************
